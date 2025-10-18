51134dc7277e753dad2c97bb437ea808b615e3fd
Kaan <ksimsek19@ku.edu.tr>
2025-10-18 22:37:25 +0300

    add jetstream connection

--- connector/connector.go
+++ connector/connector.go
@@ -1,6 +1,8 @@
 package connector
 
 import (
+	"context"
+	"errors"
 	"github.com/nats-io/nats.go"
 	"time"
 )
@@ -13,12 +15,38 @@ type Options struct {
 	Name         string
 }
 
-func Connect(options Options) (*nats.Conn, error) {
+func Connect(ctx context.Context, options Options) (*nats.Conn, error) {
 	nc, err := nats.Connect(
 		options.Url,
 	)
+
 	if err != nil {
 		return nil, err
 	}
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			nc.Close()
+		}
+	}()
+
 	return nc, nil
+}
+
+func JetStream(ctx context.Context, options Options) (nats.JetStreamContext, error) {
+	nc, err := nats.Connect(options.Url)
+	if err != nil {
+		return nil, errors.New("can not connect to nats, " + err.Error())
+	}
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			nc.Close()
+		}
+	}()
+
+	js, _ := nc.JetStream()
+	return js, nil
 }
